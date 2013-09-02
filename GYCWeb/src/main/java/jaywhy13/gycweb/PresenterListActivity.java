b3950265274fc65cb7c9015aaab17b94da7ca695
Jean-Mark Wright <jeanmark.wright@gmail.com>
2013-09-01 22:29:48 -0500

    Initial commit

---
+++ GYCWeb/src/main/java/jaywhy13/gycweb/PresenterListActivity.java
@@ -0,0 +1,14 @@
+package jaywhy13.gycweb;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by jay on 7/25/13.
+ */
+public class PresenterListActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_presenter_list);
+    }
+}
