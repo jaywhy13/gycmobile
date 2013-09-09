db6ded83a047c16d1eefece9877d9dd4f8f60104
Jean-Mark Wright <jeanmark.wright@gmail.com>
2013-09-09 09:51:58 -0500

    Added a menuable interface that allows us to quickly define what will be in the menu. This is going to be useful for static menus I'm guessing. Not sure how much we will use it for the dynamic ones. Not sure what Android provides in terms of content providers and the likes just yet.

---
+++ GYCWeb/src/main/java/jaywhy13/gycweb/screens/GYCMenuable.java
@@ -0,0 +1,27 @@
+package jaywhy13.gycweb.screens;
+
+/**
+ * Created by jay on 9/9/13.
+ */
+public interface GYCMenuable {
+
+    /**
+     * Returns a list of menu headings
+     * @return
+     */
+    public String [] getMenuHeadings();
+
+    /**
+     * Returns a list of resource ids for the images for each menu item
+     * @return
+     */
+    public int [] getMenuIcons();
+
+    /**
+     * Fired when someone clicks a menu item...
+     * @param position
+     * @param caption
+     */
+    public void menuItemClicked(int position, String caption);
+
+}
