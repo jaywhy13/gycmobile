c9eb3bed3885d21ae51003e2ac2aa71d707a3dd6
Jean-Mark Wright <jeanmark.wright@gmail.com>
2013-10-31 08:10:23 -0500

    Changed GYCPresenterList so it just extends Activity. Used a SimpleCursorAdapter to show the list of Presenters. Added some subclassable methods to allow the other listing screens to use this class like: getCursor(), getCursorAdapter(), getListModel(), getClassForListItemIntent(), modelClicked(), getPageTitle(). Started doing the same with the PresenterDetail page.

--- GYCWeb/src/main/java/jaywhy13/gycweb/screens/GYCPresenterDetail.java
+++ GYCWeb/src/main/java/jaywhy13/gycweb/screens/GYCPresenterDetail.java
@@ -2,11 +2,24 @@ package jaywhy13.gycweb.screens;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.content.ContentValues;
+import android.content.Intent;
 import android.content.res.Resources;
+import android.database.Cursor;
 import android.os.Bundle;
 
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.CursorAdapter;
+import android.widget.SimpleCursorAdapter;
+
+import jaywhy13.gycweb.GYCMainActivity;
 import jaywhy13.gycweb.fragments.GYCDetailPageFragment;
 import jaywhy13.gycweb.R;
+import jaywhy13.gycweb.models.Model;
+import jaywhy13.gycweb.models.Presenter;
+import jaywhy13.gycweb.models.Sermon;
 
 /**
  * Created by jay on 9/9/13.
@@ -18,21 +31,105 @@ public class GYCPresenterDetail extends Activity {
      */
     private GYCDetailPageFragment detailPageFragment;
 
+    Presenter presenter = new Presenter();
+    private SimpleCursorAdapter sca;
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-
+        // Grab the information from the intent and update the model
+        updateModel();
         detailPageFragment = (GYCDetailPageFragment) getFragmentManager().findFragmentById(R.id.detailPageFragment);
         detailPageFragment.setPageTitle(getPageTitle());
         detailPageFragment.setPageSubTitle(getPageSubTitle());
+
+        setupPageList();
+
     }
 
-     /**
+    /**
+     * Returns the cursor adapter that will be used for the list view in the main fragment
+     * @return
+     */
+    protected CursorAdapter getCursorAdapter(){
+        if(sca == null){
+            sca = new SimpleCursorAdapter(this, R.layout.menu_item, getCursor(), new String[]{Sermon.SERMON_TITLE},
+                    new int [] {R.id.menu_caption}, CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER);
+        }
+        return sca;
+    }
+
+    /**
+     * Returns the cursor for the main list on the page
+     * @return
+     */
+    protected Cursor getCursor(){
+        return new Presenter().getSermons(getContentResolver());
+    }
+
+    /**
+     * Sets the adapter for the list view in the main fragment and also
+     * sets up this class as a listener, so that menuItemClicked is called
+     * whenever we click an item in the menu.
+     */
+    protected void setupPageList(){
+        CursorAdapter adapter = getCursorAdapter();
+        detailPageFragment.getPageListView().setAdapter(adapter);
+        detailPageFragment.getPageListView().setOnItemClickListener(
+                new AdapterView.OnItemClickListener(){
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                        menuItemClicked(adapterView, i, view);
+                    }
+                }
+        );
+    }
+
+
+    protected Model getListModel(){
+        return new Sermon();
+    }
+
+    /**
+     * This method grabs the cursor, creates a model from it and calls modelClicked
+     * @param adapterView
+     * @param position
+     * @param view
+     */
+    protected void menuItemClicked(AdapterView adapterView, int position, View view){
+        Cursor cursor = sca.getCursor();
+        Model model = getListModel();
+        Model.cursorRowToModel(model, cursor);
+        Log.d(GYCMainActivity.TAG, "You just clicked: " + model);
+        //modelClicked(model);
+    }
+
+
+
+    /**
      * Returns a link to the detailPageFragment that contains the title, sub title, action area and list view
      * @return
      */
     public Fragment getDetailPageFragment() {
         return detailPageFragment;
+    }
+
+    /**
+     * Return the model that will be updated from the Intent
+     * @return
+     */
+    public Model getModel(){
+        return presenter;
+    }
+
+
+    /**
+     * This updates the model from the info in the Intent
+     */
+    public void updateModel(){
+        Intent intent = getIntent();
+        ContentValues values = intent.getExtras().getParcelable("model");
+        getModel().updateValues(values);
     }
 
 
@@ -42,7 +139,7 @@ public class GYCPresenterDetail extends Activity {
      * @return
      */
     public String getPageTitle() {
-        return "Pastor Rutherford";
+        return presenter.getName();
     }
 
 
--- GYCWeb/src/main/java/jaywhy13/gycweb/screens/GYCPresenterList.java
+++ GYCWeb/src/main/java/jaywhy13/gycweb/screens/GYCPresenterList.java
@@ -1,58 +1,147 @@
 package jaywhy13.gycweb.screens;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.BaseColumns;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.CursorAdapter;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 
 import jaywhy13.gycweb.GYCMainActivity;
 import jaywhy13.gycweb.R;
+import jaywhy13.gycweb.fragments.GYCListPageFragment;
+import jaywhy13.gycweb.models.Model;
+import jaywhy13.gycweb.models.Presenter;
+import jaywhy13.gycweb.util.Verses;
 
 /**
  * Created by jay on 9/9/13.
  */
-public class GYCPresenterList extends GYCMainActivity {
+public class GYCPresenterList extends Activity {
+
+    TextView pageTitle;
+    TextView pageSubTitle;
+    GYCListPageFragment listFragment;
+    SimpleCursorAdapter sca = null;
+
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
 
-        // Intialize the main stuff...
-        mainPageFragment.setPageTitle(getPageTitle());
-        mainPageFragment.setPageSubTitle(getPageSubTitle());
+        pageTitle = (TextView) findViewById(R.id.pageTitle);
+        pageSubTitle = (TextView) findViewById(R.id.pageSubTitle);
+        listFragment = (GYCListPageFragment) getFragmentManager().findFragmentById(R.id.mainPageFragment);
+
+        setupPage();
+        setupPageList();
     }
 
-    public String getPageSubTitle(){
-        return "";
+    /**
+     * Sets up the page, hides different controls and so on...
+     */
+    protected void setupPage(){
+        // Setup our page
+        pageTitle.setText(getPageTitle());
+        pageSubTitle.setText(Verses.getVerse());
+        listFragment.hideHeadings();
     }
+
+
+    /**
+     * Returns the cursor adapter that will be used for the list view in the main fragment
+     * @return
+     */
+    protected CursorAdapter getCursorAdapter(){
+        if(sca == null){
+            sca = new SimpleCursorAdapter(this, R.layout.menu_item, getCursor(), new String[]{Presenter.PRESENTER_NAME},
+                    new int [] {R.id.menu_caption}, CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER);
+        }
+        return sca;
+    }
+
+    /**
+     * Returns the cursor for the main list on the page
+     * @return
+     */
+    protected Cursor getCursor(){
+        return new Presenter().get(getContentResolver());
+    }
+
+    /**
+     * Sets the adapter for the list view in the main fragment and also
+     * sets up this class as a listener, so that menuItemClicked is called
+     * whenever we click an item in the menu.
+     */
+    protected void setupPageList(){
+        CursorAdapter adapter = getCursorAdapter();
+        listFragment.getPageListView().setAdapter(adapter);
+        listFragment.getPageListView().setOnItemClickListener(
+                new AdapterView.OnItemClickListener(){
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                        menuItemClicked(adapterView, i, view);
+                    }
+                }
+        );
+    }
+
+    /**
+     * Returns a new instance of the model that is used for the list
+     * @return
+     */
+    public Model getListModel(){
+        return new Presenter();
+    }
+
+    /**
+     * This gives us the class that should be used for defining the Intent.
+     * @return
+     */
+    public Class getClassForListItemIntent(){
+        return GYCPresenterDetail.class;
+    }
+
+    /**
+     * This is called when a list item is clicked. menuItemClicked creates the model object and
+     * calls this class.
+     * @param model
+     */
+    public void modelClicked(Model model){
+        Intent intent = new Intent(this, getClassForListItemIntent());
+        intent.putExtra(BaseColumns._ID, model.getId());
+        intent.putExtra("table_name", model.getTableName());
+        intent.putExtra("model", model.getValues());
+        startActivity(intent);
+    }
+
+    /**
+     * This method grabs the cursor, creates a model from it and calls modelClicked
+     * @param adapterView
+     * @param position
+     * @param view
+     */
+    protected void menuItemClicked(AdapterView adapterView, int position, View view){
+        Cursor cursor = sca.getCursor();
+        Model model = getListModel();
+        Model.cursorRowToModel(model, cursor);
+        Log.d(GYCMainActivity.TAG, "You just clicked: " + model);
+        modelClicked(model);
+    }
+
 
     /**
      * Returns the page title
      * @return
      */
     public String getPageTitle(){
-        return getResources().getString(R.string.presenter_list_page_title);
+        return getString(R.string.presenter_list_page_title);
     }
 
-    @Override
-    public int[] getMenuIcons() {
-        return new int [] {
-            R.drawable.cloud_icon, R.drawable.cloud_icon};
-    }
-
-    @Override
-    public String[] getMenuHeadings() {
-        return new String [] {"Item One", "Item Two"};
-    }
-
-    @Override
-    public void menuItemClicked(int position, String caption) {
-        Intent intent = new Intent(this, GYCPresenterDetail.class);
-        startActivity(intent);
-    }
-
-    /**
-     * Returns the page list in the fragment
-     * @return
-     */
-    public ListView getPageList(){
-        return mainPageFragment.getPageListView();
-    }
 }
