0305a9a0f93a9ae13fe038e0a958979336984a4b
Jean-Mark Wright <jeanmark.wright@gmail.com>
2013-11-17 16:16:56 -0500

    Added a Verses class.

---
+++ GYCWeb/src/main/java/jaywhy13/gycweb/util/Verses.java
@@ -0,0 +1,12 @@
+package jaywhy13.gycweb.util;
+
+/**
+ * Created by Jay on 10/28/13.
+ */
+public class Verses {
+
+    public static String getVerse(){
+        return "Thy word have I hid in my heart that I might not sin against thee. \nPs 119:11";
+    }
+
+}
