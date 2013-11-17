d11e6963c782e21da87c6c2684c21aa2f164c91a
Jean-Mark Wright <jeanmark.wright@gmail.com>
2013-11-17 16:04:49 -0500

    Implemented the overrides in GYCSermonList so we changed where the cursor and adapter points to.

--- GYCWeb/src/main/java/jaywhy13/gycweb/screens/GYCSermonList.java
+++ GYCWeb/src/main/java/jaywhy13/gycweb/screens/GYCSermonList.java
@@ -1,9 +1,16 @@
 package jaywhy13.gycweb.screens;
 
 import android.app.Activity;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CursorAdapter;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 
 import jaywhy13.gycweb.R;
+import jaywhy13.gycweb.models.Model;
+import jaywhy13.gycweb.models.Sermon;
 
 /**
  * Created by jay on 9/9/13.
@@ -16,5 +23,29 @@ public class GYCSermonList extends GYCPresenterList {
     @Override
     public String getPageTitle() {
         return getResources().getString(R.string.sermon_list_page_title);
+    }
+
+    @Override
+    protected Cursor getCursor() {
+        return new Sermon().get(getContentResolver());
+    }
+
+    @Override
+    protected CursorAdapter getCursorAdapter() {
+        if(sca == null){
+            sca = new SimpleCursorAdapter(this, R.layout.menu_item, getCursor(), new String[]{Sermon.SERMON_TITLE, Sermon.SERMON_PRESENTER_NAME, Sermon.SERMON_DURATION},
+                    new int [] {R.id.menu_caption, R.id.menu_sub_caption, R.id.menu_sub_caption_right}, CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER);
+        }
+        return sca;
+    }
+
+    @Override
+    public Model getListModel() {
+        return new Sermon();
+    }
+
+    @Override
+    public Class getClassForListItemIntent() {
+        return GYCSermonDetail.class;
     }
 }
