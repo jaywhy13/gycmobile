dcdd1d406fa5009fb4fbc435056b7d4053ee21eb
Jean-Mark Wright <jeanmark.wright@gmail.com>
2013-11-17 16:04:17 -0500

    Changed GYCSermonDetail so it's not showing random text. It's now pulling info from the intent.

--- GYCWeb/src/main/java/jaywhy13/gycweb/screens/GYCSermonDetail.java
+++ GYCWeb/src/main/java/jaywhy13/gycweb/screens/GYCSermonDetail.java
@@ -2,22 +2,33 @@ package jaywhy13.gycweb.screens;
 
 import android.app.Activity;
 import android.os.Bundle;
+import jaywhy13.gycweb.models.Model;
+import jaywhy13.gycweb.models.Sermon;
 
 /**
  * Created by jay on 9/9/13.
  */
 public class GYCSermonDetail extends GYCPresenterDetail {
+
+    Sermon sermon = new Sermon();
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
     @Override
     public String getPageTitle() {
-        return "Just friends";
+        return sermon.getTitle();
     }
 
     @Override
     public String getPageSubTitle() {
-        return "Sis E. Parker";
+        return String.valueOf(sermon.getPresenterId());
+    }
+
+    @Override
+    public Model getModel() {
+        return sermon;
     }
 }
+
