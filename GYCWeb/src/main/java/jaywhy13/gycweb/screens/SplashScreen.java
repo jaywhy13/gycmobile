b716d3dabb330a190320e0a8994fc71087911d8a
Jean-Mark Wright <jeanmark.wright@gmail.com>
2013-12-11 00:29:58 -0500

    Adjusted the splash screen. Dismissed itself after the SyncTask is complete.

--- GYCWeb/src/main/java/jaywhy13/gycweb/screens/SplashScreen.java
+++ GYCWeb/src/main/java/jaywhy13/gycweb/screens/SplashScreen.java
@@ -1,11 +1,15 @@
 package jaywhy13.gycweb.screens;
 
+import jaywhy13.gycweb.GYCMainActivity;
 import jaywhy13.gycweb.R;
+import jaywhy13.gycweb.media.GYCMedia;
 import jaywhy13.gycweb.screens.util.SystemUiHider;
 import jaywhy13.gycweb.tasks.InitTask;
+import jaywhy13.gycweb.tasks.SyncTask;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
@@ -25,9 +29,18 @@ public class SplashScreen extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
 
-        InitTask task = new InitTask(this);
-        task.execute();
+        Intent mediaPlayerServiceIntent = new Intent(this, GYCMedia.class);
+        startService(mediaPlayerServiceIntent);
+
+        SyncTask syncTask = new SyncTask(this){
+            @Override
+            protected void onPostExecute(Object o) {
+                Intent intent = new Intent(SplashScreen.this, GYCMainActivity.class);
+                finish();
+                startActivity(intent);
+            }
+        };
+        syncTask.execute();
 
     }
-
 }
