7e530dc4f46b4ba9d763f58d0fd3ce9c4b025f26
Jean-Mark Wright <jeanmark.wright@gmail.com>
2013-12-11 00:27:55 -0500

    Stopped using the detailPageFragment headings... hid them and just used the titles on the page. Also started toying with the display of the sidebar. Did some code here to connect to the MediaService as well.

--- GYCWeb/src/main/java/jaywhy13/gycweb/screens/GYCPresenterDetail.java
+++ GYCWeb/src/main/java/jaywhy13/gycweb/screens/GYCPresenterDetail.java
@@ -2,21 +2,34 @@ package jaywhy13.gycweb.screens;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.app.LoaderManager;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.Loader;
+import android.content.ServiceConnection;
 import android.content.res.Resources;
 import android.database.Cursor;
 import android.os.Bundle;
 
+import android.os.IBinder;
+import android.provider.BaseColumns;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.CursorAdapter;
+import android.widget.LinearLayout;
 import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import jaywhy13.gycweb.GYCMainActivity;
 import jaywhy13.gycweb.fragments.GYCDetailPageFragment;
 import jaywhy13.gycweb.R;
+import jaywhy13.gycweb.media.GYCMedia;
 import jaywhy13.gycweb.models.Model;
 import jaywhy13.gycweb.models.Presenter;
 import jaywhy13.gycweb.models.Sermon;
@@ -24,7 +37,81 @@ import jaywhy13.gycweb.models.Sermon;
 /**
  * Created by jay on 9/9/13.
  */
-public class GYCPresenterDetail extends Activity {
+public class GYCPresenterDetail extends Activity implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    private MediaReceiver mediaReceiver;
+
+    private TextView pageTitle;
+    private TextView pageSubTitle;
+    private TextView pageDescription;
+
+
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        // Grab the information from the intent and update the model
+        updateModel();
+
+        detailPageFragment = (GYCDetailPageFragment) getFragmentManager().findFragmentById(R.id.detailPageFragment);
+        detailPageFragment.hideHeadings();
+
+        pageTitle = (TextView) findViewById(R.id.pageTitle);
+        pageSubTitle = (TextView) findViewById(R.id.pageSubTitle);
+        pageDescription = (TextView) findViewById(R.id.pageDescription);
+
+        sidebar = (LinearLayout) findViewById(R.id.sidebar);
+
+
+        setupPageTitles();
+        setupPageList();
+        setupActionArea();
+
+        mediaReceiver = new MediaReceiver(){
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                onMediaPlayerIntent(intent);
+            }
+        };
+
+        getLoaderManager().initLoader(1, null, this);
+
+    }
+
+
+    /**
+     * Sets the title, sub title and hides the page description text view
+     */
+    protected void setupPageTitles(){
+        int numSermons = presenter.getNumSermons();
+        String subTitle = numSermons + " sermon" + (numSermons == 1 ? "" : "s");
+
+        pageTitle.setText(presenter.getName());
+        pageSubTitle.setText(subTitle);
+        pageDescription.setVisibility(View.GONE);
+        detailPageFragment.getListCaption().setVisibility(View.GONE);
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Add a broadcast listener..
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(GYCMedia.ACTION_LOAD);
+        intentFilter.addAction(GYCMedia.ACTION_PLAY);
+        intentFilter.addAction(GYCMedia.ACTION_PAUSE);
+        intentFilter.addAction(GYCMedia.ACTION_STOP);
+        registerReceiver(mediaReceiver, intentFilter);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(mediaReceiver);
+    }
+
+    private LinearLayout sidebar;
 
     /**
      * The fragment that contains the entire page basically...
@@ -32,20 +119,49 @@ public class GYCPresenterDetail extends Activity {
     private GYCDetailPageFragment detailPageFragment;
 
     Presenter presenter = new Presenter();
-    private SimpleCursorAdapter sca;
+    protected SimpleCursorAdapter sca;
 
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail);
-        // Grab the information from the intent and update the model
-        updateModel();
-        detailPageFragment = (GYCDetailPageFragment) getFragmentManager().findFragmentById(R.id.detailPageFragment);
-        detailPageFragment.setPageTitle(getPageTitle());
-        detailPageFragment.setPageSubTitle(getPageSubTitle());
+    private GYCMedia.GYCMediaPlayer mediaPlayer;
+    private boolean serviceBound = false;
 
-        setupPageList();
+    ServiceConnection serviceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            mediaPlayer = (GYCMedia.GYCMediaPlayer) iBinder;
+            serviceBound = true;
 
+            Log.d(GYCMainActivity.TAG, "Service connected... adding music sidebar...");
+
+            // Check if we need to add the fragment
+            GYCMedia.addMusicSidebar(mediaPlayer, GYCPresenterDetail.this, sidebar);
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            mediaPlayer = null;
+            serviceBound = false;
+        }
+    };
+
+    /**
+     * This method is called to establish a connection to the GYCMedia service
+     */
+    public void connectToMediaService(){
+        if(!serviceBound){
+            Intent intent = new Intent(this, GYCMedia.class);
+            bindService(intent, serviceConnection, BIND_AUTO_CREATE);
+        }
     }
+
+    public void disconnectFromMediaService(){
+        unbindService(serviceConnection);
+    }
+
+    public boolean isServiceBound() {
+        return serviceBound;
+    }
+
+
 
     /**
      * Returns the cursor adapter that will be used for the list view in the main fragment
@@ -53,18 +169,25 @@ public class GYCPresenterDetail extends Activity {
      */
     protected CursorAdapter getCursorAdapter(){
         if(sca == null){
-            sca = new SimpleCursorAdapter(this, R.layout.menu_item, getCursor(), new String[]{Sermon.SERMON_TITLE},
-                    new int [] {R.id.menu_caption}, CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER);
+            sca = new SimpleCursorAdapter(this, R.layout.menu_item, null, new String[]{Sermon.SERMON_TITLE, Sermon.SERMON_PRESENTER_NAME},
+                    new int [] {R.id.menu_caption, R.id.menu_sub_caption}, CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER);
         }
         return sca;
+    }
+
+    /**
+     * Adds content to the action area of the page
+     */
+    public void setupActionArea(){
+
     }
 
     /**
      * Returns the cursor for the main list on the page
      * @return
      */
-    protected Cursor getCursor(){
-        return new Presenter().getSermons(getContentResolver());
+    protected Loader<Cursor> getCursorLoader(){
+        return presenter.getSermons(this);
     }
 
     /**
@@ -101,7 +224,28 @@ public class GYCPresenterDetail extends Activity {
         Model model = getListModel();
         Model.cursorRowToModel(model, cursor);
         Log.d(GYCMainActivity.TAG, "You just clicked: " + model);
-        //modelClicked(model);
+        modelClicked(model);
+    }
+
+    /**
+     * This gives us the class that should be used for defining the Intent.
+     * @return
+     */
+    public Class getClassForListItemIntent(){
+        return GYCSermonDetail.class;
+    }
+
+    /**
+     * This is called when a list item is clicked. menuItemClicked creates the model object and
+     * calls this class.
+     * @param model
+     */
+    public void modelClicked(Model model){
+        Intent intent = new Intent(this, getClassForListItemIntent());
+        intent.putExtra(BaseColumns._ID, model.getId());
+        intent.putExtra("table_name", model.getTableName());
+        intent.putExtra("model", model.getValues());
+        startActivity(intent);
     }
 
 
@@ -110,7 +254,7 @@ public class GYCPresenterDetail extends Activity {
      * Returns a link to the detailPageFragment that contains the title, sub title, action area and list view
      * @return
      */
-    public Fragment getDetailPageFragment() {
+    public GYCDetailPageFragment getDetailPageFragment() {
         return detailPageFragment;
     }
 
@@ -133,13 +277,8 @@ public class GYCPresenterDetail extends Activity {
     }
 
 
-    /**
-     * This is used to set the title of the page
-     *
-     * @return
-     */
-    public String getPageTitle() {
-        return presenter.getName();
+    public TextView getPageTitle() {
+        return pageTitle;
     }
 
 
@@ -148,10 +287,78 @@ public class GYCPresenterDetail extends Activity {
      *
      * @return
      */
-    public String getPageSubTitle() {
-        Resources resources = getResources();
-        return resources.getString(R.string.presenter_list_page_title);
+    public TextView getPageSubTitle() {
+        return pageSubTitle;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        try {
+            unbindService(serviceConnection);
+        } catch(IllegalArgumentException iae){
+            // in case we didn't bind yet
+        }
+    }
+
+    public GYCMedia.GYCMediaPlayer getPlayer(){
+        return mediaPlayer;
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        return getCursorLoader();
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
+        sca.swapCursor(cursor);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> cursorLoader) {
+        sca.swapCursor(null);
+    }
+
+    public static class MediaReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+        }
+    }
+
+    protected void onMediaPlayerIntent(Intent intent){
+        Bundle extras = intent.getExtras();
+        if(intent.getAction().equals(GYCMedia.ACTION_LOAD)){
+            trackLoading(extras);
+        } else if(intent.getAction().equals(GYCMedia.ACTION_PLAY)){
+            trackPlayed(extras);
+        } else if(intent.getAction().equals(GYCMedia.ACTION_PAUSE)){
+            trackPaused(extras);
+        } else if(intent.getAction().equals(GYCMedia.ACTION_STOP)){
+            trackStopped(extras);
+        }
+    }
+
+    protected void trackLoading(Bundle extras){
+
+    }
+
+    protected void trackPlayed(Bundle extras){
+
+    }
+
+    protected void trackPaused(Bundle extras){
+
+    }
+
+    protected void trackStopped(Bundle extras){
+
     }
 
 
+    public TextView getPageDescription() {
+        return pageDescription;
+    }
 }
